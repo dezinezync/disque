1acc25b811582f9f8cbc81dd67fde4534d567928
Nikhil Nigade <nikhil@dezinezync.com>
2019-11-15 22:49:12 +0530

    updated to use node_redis

--- index.js
+++ index.js
@@ -1,139 +1,151 @@
 'use strict';
 
-const hiredis = require('hiredis');
-
+const redis = require('redis');
 const slice   = Array.prototype.slice;
-const reader  = new hiredis.Reader();
+const {promisify} = require('util');
 
 class Disque {
-  constructor(config) {
-    if (config instanceof Function)
-      this.config = config;
-    else
-      this.config = function() { return config || {} };
-  }
+  
+    constructor(config) {
+        if (config instanceof Function)
+            this.config = config;
+        else
+            this.config = function() { return config || {} };
+    }
 
-  connect() {
-    if (this.socket)
-      return Promise.resolve(this.socket);
-    else {
-      return Promise.resolve(this.config())
-        .then(config => {
-          const addr  = config.nodes[0];
-          const parts = addr.split(':');
-          this.socket = hiredis.createConnection(parts[1], parts[0]);
-          this.socket
-            .on('reply', data => {
-              if (data instanceof Error)
-                this._operations.shift()[1](data);
-              else
-                this._operations.shift()[0](data);
+    connect() {
+
+        let config;
+
+        if (this.sendAsync) {
+            return Promise.resolve(this.sendAsync);
+        }
+        else {
+            return Promise.resolve(this.config())
+            .then(result => {
+
+                config = result;
+
+                const addr  = config.nodes[0];
+                const parts = addr.split(':');
+                this.client = redis.createClient(parts[1], parts[0]);
+
+                this.sendAsync = promisify(this.client.send_command).bind(this.client);
+
+                return Promise.resolve(this.sendAsync);
+
             })
-            .on('error', error => {
-              this._operations.shift()[1](error);
+            .then(() => {
+
+                if (config.auth) {
+                    return this.call('auth', config.auth);
+                }
+
+                return this.sendAsync;
+
             });
-          this._operations = [];
+        }
 
-          if (config.auth)
-            return this.call('auth', config.auth);
+    }
+
+    call (command, args) {
+
+        console.debug(command, args);
+
+        return this.connect()
+        .then((sendAsync) => {
+            return sendAsync(command, args);
         });
     }
-  }
 
-  call() {
-    return this.connect()
-      .then(() => {
-        return new Promise((resolve, reject) => {
-          this._operations.push([ resolve, reject ]);
-          this.socket.write.apply(this.socket, arguments);
-        });
-      });
-  }
+    addjob(queue, job, options) {
+        if (options) {
+            const timeout = options.timeout || 0;
+            const keys    = Object.keys(options);
+            const args    = keys
+                .filter(key => key !== 'timeout')
+                .map(pairify(options))
+                .reduce((accum, pair) => accum.concat(pair), []);
 
-  addjob(queue, job, options) {
-    if (options) {
-      const timeout = options.timeout || 0;
-      const keys    = Object.keys(options);
-      const args    = keys
-        .filter(key => key !== 'timeout')
-        .map(pairify(options))
-        .reduce((accum, pair) => accum.concat(pair), []);
-
-      return this.call.apply(this, [ 'ADDJOB', queue, job, timeout ].concat(args));
+            return this.call('ADDJOB', [queue, job, timeout ].concat(args));
+        }
+        else
+            return this.call('addjob', [queue, job, 0]);
     }
-    else
-      return this.call('addjob', queue, job, 0);
-  }
 
-  getjob(queue, options) {
-    const keys    = Object.keys(options || {});
-    const args    = keys
-      .map(pairify(options))
-      .reduce((accum, pair) => accum.concat(pair), []);
+    getjob(queue, options) {
+        const keys    = Object.keys(options || {});
+        const args    = keys
+            .map(pairify(options))
+            .reduce((accum, pair) => accum.concat(pair), []);
 
-    args.push('from', queue);
+        args.push('from', ...queue);
 
-    return this.call.apply(this, [ 'GETJOB' ].concat(args))
-      .then(function(jobs) {
-        if (!jobs)
-          return []
-        
-        return jobs.map(function(job) {
-          return {
-            queue: job[0],
-            id:    job[1],
-            body:  job[2]
-          };
+        return this.call('GETJOB', args)
+        .then(function(jobs) {
+
+            if (!jobs) {
+                return []
+            }
+
+            return jobs.map(function(job) {
+                return {
+                    queue: job[0],
+                    id:    job[1],
+                    body:  job[2]
+                };
+            });
         });
-      });
-  }
 
-  ackjob(ids) {
-    
-    if (!(ids instanceof Array)) 
-      ids = [ids];
-
-    return this.call('ACKJOB', ...ids)
-  }
-
-  info() {
-    return this.call('info')
-      .then(parseInfo);
-  }
-
-  end() {
-    if (this.socket) {
-      this.socket.end();
-      this.socket = null;
     }
-  }
+
+    ackjob(ids) {
+
+        if (!(ids instanceof Array)) 
+            ids = [ids];
+
+        return this.call('ACKJOB', ...ids)
+
+    }
+
+    info() {
+        return this.call('info')
+        .then(parseInfo);
+    }
+
+    end() {
+        if (this.client) {
+            this.client.quit();
+            this.client = null;
+        }
+    }
 }
 
 function parseInfo(str) {
-  const result = {};
+    const result = {};
 
-  str
+    str
     .split("\r\n")
     .forEach(function(line) {
-      if (line.length === 0 || line[0] === '#') return;
+        if (line.length === 0 || line[0] === '#') return;
 
-      const parts = line.split(':');
-      const key   = parts[0];
-      const value = parts[1];
+        const parts = line.split(':');
+        const key   = parts[0];
+        const value = parts[1];
 
-      result[key] = value;
+        result[key] = value;
     });
 
-  return result;
+    return result;
 }
 
 function pairify(obj) {
-  return function(key) {
-    if (obj[key] === true)
-      return [ key ];
-    else
-      return [ key, obj[key] ];
-  };
+    return function(key) {
+        if (obj[key] === true)
+            return [ key ];
+        else
+            return [ key, obj[key] ];
+    };
 }
 
 module.exports = Disque;
--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 'use strict';
 
-const test   = require('tape-promise')(require('tape'));
-const Disq   = require('./index');
-const mock   = require('./mock');
+const tape = require('tape')
+const _test = require('tape-promise').default // <---- notice 'default'
+const test = _test(tape) // decorate tape
 
-const NODES   = [ '127.0.0.1:7712', '127.0.0.1:7713' ];
+const Disq   = require('./index');
+
+const NODES   = [ '192.168.1.110:7711' ];
 const CYCLE   = 5;
 const OPTIONS = { cycle: CYCLE };
 
@@ -49,79 +51,6 @@ test('getjob', function(t) {
       t.equal(jobs[0].queue, 'q1');
       t.assert(jobs[0].id.startsWith('D-'));
       t.equal(jobs[0].body, 'j1');
-    });
-});
-
-test('addjob with options', function(t) {
-  let args;
-
-  const disque = new Disq({ nodes: [ '127.0.0.1:7714' ] });
-
-  const server = mock({
-    addjob() {
-      args = Array.prototype.slice.call(arguments);
-      return 'D-123';
-    }
-  }).listen(7714);
-
-  return disque.addjob('q1', 'j1', { timeout: 1000, ttl: 60, async: true })
-    .then(function() {
-      t.deepEqual(args, [ 'q1', 'j1', '1000', 'ttl', '60', 'async' ]);
-    })
-    .then(function() {
-      disque.end();
-      server.close();
-    });
-});
-
-test('getjob with options', function(t) {
-  let args;
-
-  const disque = new Disq({ nodes: [ '127.0.0.1:7714' ] });
-
-  const server = mock({
-    getjob() {
-      args = Array.prototype.slice.call(arguments);
-      return [[ 'q1', 'D-123', 'j1' ]];
-    }
-  }).listen(7714);
-
-  return disque.getjob('q1', { count: 1, nohang: true })
-    .then(function() {
-      t.deepEqual(args, [ 'count', '1', 'nohang', 'from', 'q1' ]);
-    })
-    .then(function() {
-      disque.end();
-      server.close();
-    });
-});
-
-test('authentication', function(t) {
-  let authenticated = false;
-
-  const disque = new Disq({ nodes: [ '127.0.0.1:7714' ], auth: 's3cr3t' });
-
-  const server = mock({
-    ping() {
-      if (authenticated)
-        return 'LEGIT';
-      else
-        return 'MEH';
-    },
-
-    auth(password) {
-      authenticated = (password === 's3cr3t');
-      return 'OK';
-    }
-  }).listen(7714);
-
-  return disque.call('ping')
-    .then(function(reply) {
-      t.equal(reply, 'LEGIT');
-    })
-    .then(function() {
-      disque.end();
-      server.close();
     });
 });
 
